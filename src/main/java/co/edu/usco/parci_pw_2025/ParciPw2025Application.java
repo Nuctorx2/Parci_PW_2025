ea88dff51a638a7638a6d32d4a03f68a7f25a993
Juan Alejandro Rojas Vargas <u20231210916@usco.edu.co>
2025-04-29 09:46:39 -0500

    Initial commit

---
+++ src/main/java/co/edu/usco/parci_pw_2025/ParciPw2025Application.java
@@ -0,0 +1,13 @@
+package co.edu.usco.parci_pw_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParciPw2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParciPw2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/usco/parci_pw_2025/ParciPw2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usco.parci_pw_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParciPw2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
